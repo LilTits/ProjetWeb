a8857edfaad724740c19702fcb9d2964337568cb
Titouan <guiochettitouan@outlook.com>
2019-11-07 00:28:05 +0100

    Navbar

--- ProjetWeb/resources/views/inc/header.blade.php
+++ ProjetWeb/resources/views/inc/header.blade.php
@@ -1,18 +1,19 @@
 <!-- Début section navigation -->
-<header id="navigation" class="u-full-width">
-    <nav class="bar_nav container">
-        <a class="button" href="/">Panier</a>
-        <ul class="liens_nav" role="navigation">
-            <li><a href="/">Welcome</a></li>
-            <li><a href="/product">Products</a></li>
-            <li><a href="/event">Events</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-        <a class="button" href="/login">login/Register</a>
-        <div class="burger_nav">
-            <div class="ligne1"></div>
-            <div class="ligne2"></div>
-            <div class="ligne3"></div>
+<header>
+    <nav id="navigation" class="u-full-width bar_nav">
+        <div class="container">
+            <ul>
+                <li class="u-pull-left"><a href="/">Panier</a></li>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/product">Produits</a></li>
+                <li><a href="/event">Evènements</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li class="u-pull-right"><a href="/login">S'inscrire/Se Connecter</a></li>
+                <li class="icon">
+                    <a href="javascript:void(0);" onclick="toggleMobileNav();">&#9776;</a>
+                </li>
+                <div class="u-cf"></div>
+            </ul>
         </div>
     </nav>
 </header>
