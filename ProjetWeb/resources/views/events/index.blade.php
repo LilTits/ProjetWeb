3869017ab74fe556baa44de1e5a6889592aba153
Titouan <guiochettitouan@outlook.com>
2019-11-13 09:12:53 +0100

    Reviews

--- ProjetWeb/resources/views/events/index.blade.php
+++ ProjetWeb/resources/views/events/index.blade.php
@@ -14,9 +14,11 @@
                     <h5 class="card-title">{{$event->name}}</h5>
                     <h6 class="price">{{$event->price}}</h6>
                     <p class="card-text">{{$event->description}}</p>
+                    <p class="card-text">Débute le : {{$event->start_date}}</p>
+                    {{-- <p class="card-text">Et termine le : {{$event->end_date}}</p> --}}
                     <a href="/events/{{$event->event_id}}" class="btn btn-primary buy_button_event">En savoir plus</a>
                     <br>
-                <small>Ajouté le {{$event->created_at}} by {{$event->creator}}</small>
+                    <small>Ajouté le {{$event->created_at}} by {{$event->creator}}</small>
                 </div>
             </div>
             @endforeach
