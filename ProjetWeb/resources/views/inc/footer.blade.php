175ed739aeeeca68eda849564a5e394f128c7417
Titouan <guiochettitouan@outlook.com>
2019-11-14 14:50:57 +0100

    Cookies

--- ProjetWeb/resources/views/inc/footer.blade.php
+++ ProjetWeb/resources/views/inc/footer.blade.php
@@ -1,7 +1,12 @@
 <!-- Début footer -->
 <footer id="footer">
     <div id="term" class="container">
-        <h5><a href="/legale">Mentions Légales & Politique de Protection des Données Personnelles & Conditions générales de vente</a></h5>
+        <button type="button" class="btn btn-default" id="test1" data-toggle="modal" data-target="#exampleModal">
+            Cookies
+        </button>
+        @include('inc.cookie')
+        <h5><a href="/legale">Mentions Légales & Politique de Protection des Données Personnelles & Conditions générales
+                de vente</a></h5>
     </div>
     <div id="credits">
         <p>Créé par Varjavandi Thomas, Brisé Mathéo et Guioochet Titouan</p>
